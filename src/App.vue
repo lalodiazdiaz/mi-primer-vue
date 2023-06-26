b558dda9507411abf7f851bc6130b9c76d451ce7
lalodiazdiaz <eduardodiazdiaz93@gmial.com>
2023-06-26 16:44:06 -0600

    counter finished

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,84 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { computed, ref } from "vue";
+
+const name = "Vue dinamico";
+
+const counter = ref(0);
+
+const increment = () => {
+  counter.value++;
+};
+const decrememt = () => {
+  counter.value--;
+};
+const reset = () => {
+  counter.value = 0;
+};
+
+const blockBtn = computed(() => {
+  const numSearch = favNumber.value.find((num) => num === counter.value);
+  console.log(numSearch);
+  // if (numSearch === 0) {
+  //   return true;
+  // }
+  // return numSearch ? true : false;
+  return numSearch || numSearch === 0;
+});
+
+const classCounter = computed(() => {
+  if (counter.value === 0) {
+    return "zero";
+  }
+  if (counter.value > 0) {
+    return "positive";
+  }
+  if (counter.value < 0) {
+    return "negative";
+  }
+});
+const favNumber = ref([]);
+
+const add = () => {
+  favNumber.value.push(counter.value);
+};
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+  <div class="container text-center mt-3">
+    <h1>{{ name.toUpperCase() }}</h1>
+    <h2 :class="classCounter">{{ counter }}</h2>
+    <div class="btn-group">
+      <button @click="increment" class="btn btn-success">Increment</button>
+      <button @click="decrememt" class="btn btn-danger">Decrement</button>
+      <button @click="reset" class="btn btn-secondary">Reset</button>
+      <button @click="add" :disabled="blockBtn" class="btn btn-primary">
+        Add
+      </button>
     </div>
-  </header>
 
-  <main>
-    <TheWelcome />
-  </main>
+    <ul class="list-group mt-4">
+      <li
+        class="list-group-item"
+        v-for="(numero, index) in favNumber"
+        :key="index"
+      >
+        {{ numero }}
+      </li>
+    </ul>
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
+<style>
+h1 {
+  color: red;
 }
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+.positive {
+  color: green;
 }
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+.negative {
+  color: red;
+}
+.zero {
+  color: peru;
 }
 </style>
